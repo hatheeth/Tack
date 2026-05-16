88c66cc02cc1fbffd1666c91f5cffe3d4bfeacd9
hatheeth <hathithinsan02@gmail.com>
2026-05-16 13:39:54 +0530

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    alias(libs.plugins.android.application) apply false
-
-    // 🔑 Fix: Explicitly add the version here so Gradle can find and download it
-    id("com.google.gms.google-services") version "4.4.1" apply false
-}
